7d54c23d3d0473cfe4dd64fc2e095ffcd3435c76
banhdauxanhh <narkarot1234@gmail.com>
2026-01-03 02:57:51 +0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,104 @@
+const subjects = {
+  "Tin học đại cương": [
+    {
+      question: "Trong Excel, hàm MOD trả về kết quả gì?",
+      options: [
+        "Phần dư của phép chia giữa hai số",
+        "Giá trị nhỏ nhất trong một tập hợp số",
+        "Giá trị lớn nhất trong một tập hợp số",
+        "Số nguyên sau khi chia hai số"
+      ],
+      answer: 0
+    },
+    {
+      question: "Kết quả của công thức =RIGHT(\"Microsoft\", 4) là gì?",
+      options: ["Micr", "Micro", "soft", "sof"],
+      answer: 2
+    },
+    {
+      question: "Để thêm các ghi chú vào một ô trong Excel, sử dụng thao tác nào?",
+      options: [
+        "Data > Add Note",
+        "Home > Insert Note",
+        "Insert > Comment",
+        "View > Add Annotation"
+      ],
+      answer: 2
+    },
+    {
+      question: "Khối nào trong CPU có nhiệm vụ giải mã các lệnh?",
+      options: [
+        "Tập các thanh ghi",
+        "Khối điều khiển",
+        "Hệ thống vào-ra",
+        "Khối tính toán số học và logic"
+      ],
+      answer: 1
+    },
+    {
+      question: "Tính năng nào trong Word giúp bạn tạo bảng mục lục tự động?",
+      options: [
+        "Layout > Index",
+        "Insert > Index",
+        "References > Table of Contents",
+        "Review > Table of Contents"
+      ],
+      answer: 2
+    },
+
+    {
+      question: "Tường lửa (firewall) có vai trò gì trong bảo mật?",
+      options: [
+        "Mở các trang web bị chặn",
+        "Tăng tốc độ tải tệp",
+        "Chặn các kết nối không an toàn từ Internet",
+        "Kiểm soát thông tin trên mạng xã hội"
+      ],
+      answer: 2
+    },
+
+    {
+      question: "Phần mềm độc hại (malware) có thể lây nhiễm qua cách nào?",
+      options: [
+        "Chơi trò chơi trên máy tính",
+        "Tắt máy tính",
+        "Tải tệp tin từ các trang web không tin cậy",
+        "Sử dụng email"
+      ],
+      answer: 2
+    },
+
+    {
+      question: "Mã độc tống tiền (ransomware) hoạt động như thế nào?",
+      options: [
+        "Cải thiện bảo mật hệ thống",
+        "Chặn quảng cáo",
+        "Tăng tốc độ Internet",
+        "Mã hóa dữ liệu và yêu cầu tiền chuộc"
+      ],
+      answer: 3
+    },
+
+    {
+      question: "Phishing là gì?",
+      options: [
+        "Dịch vụ chia sẻ tệp",
+        "Phương pháp bảo mật",
+        "Phần mềm chống virus",
+        "Kỹ thuật lừa đảo nhằm đánh cắp thông tin cá nhân"
+      ],
+      answer: 3
+    },
+
+    {
+      question: "Phím tắt để khóa màn hình trong Windows 10 là gì?",
+      options: [
+        "Windows + K",
+        "Windows + L",
+        "Ctrl + K",
+        "Ctrl + L"
+      ],
+      answer: 1
+    }
+  ]
+};
